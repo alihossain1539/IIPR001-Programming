694776f1ff437a68a65d6a7d3b767821abf259e4
alihossain1539 <alihossain15395@gmail.com>
2023-10-26 23:48:06 +1100

    initial commit
    initial commit of game

---
+++ Jedi.java
@@ -0,0 +1,35 @@
+public class Jedi {
+    private String name;
+    private int health;
+    private boolean isLightSaberOn;
+
+    // Constructor
+    public Jedi(String name, int health) {
+        this.name = name;
+        this.health = health;
+        this.isLightSaberOn = false;
+    }
+
+    // Methods
+    public void activateLightSaber() {
+        isLightSaberOn = true;
+        System.out.println(name + " activates their lightsaber!");
+    }
+
+    public void attack() {
+        if (isLightSaberOn) {
+            System.out.println(name + " attacks with the lightsaber.");
+        } else {
+            System.out.println(name + " tries to attack but the lightsaber is off.");
+        }
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        System.out.println(name + " takes " + damage + " damage.");
+    }
+
+    public int getHealth() {
+        return health;
+    }
+}
